79d41c94c38574afcc45f8ca9b342672bbee0651
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-08-18 01:15:35 +0530

    index page

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,50 @@
-import { Link } from "expo-router";
+import { Link, Redirect, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-sky-100">
-      <Text className="text-3xl font-pblack">Hello</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: "blue" }}>
-        Go to home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full flex justify-center items-center min-h-[85vh] px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover endless possibilities with{" "}
+              <Text className="text-secondary-200 underline">Aora</Text>
+            </Text>
+            {/* <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            /> */}
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            WHere creativity meets innovation: embark on a journey of limitless
+            exploration with Aora
+          </Text>
+          {/* ============================================== */}
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
